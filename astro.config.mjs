e0516c336a82c236833573417ef8c0d5bcd29a4b
seekpower <1248832035@qq.com>
2026-01-09 21:56:02 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'www.seekpower.top',
+    site: 'https://github.com/seekpower/seekpower.github.io',
 });
